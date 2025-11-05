29f14633a595edd4264842a166bcc59268b92492
KhaiNP101104 <khainpse183590@fpt.edu.vn>
2025-11-05 08:44:17 +0700

    Delete SWP201c directory

--- SWP201c/src/pages/Driver/Vehicles/hooks/index.js
+++
@@ -1,7 +0,0 @@
-// Driver/Vehicles/hooks/index.js
-// Barrel export for vehicles hooks
-
-export * from './useVehiclesData';
-export * from './useVehicleModals';
-export * from './useVehicleForm';
-// hooks barrel should not re-export components or utils; move to correct barrels
